0f71d2038010e0145fa53879caa1544df129ac20
r1nb0 <159806721+r1nb0@users.noreply.github.com>
2024-03-27 23:41:23 +0300

    Update main.c

--- OSiSP/lab8/client/main.c
+++ OSiSP/lab8/client/main.c
@@ -5,22 +5,63 @@
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <stdbool.h>
-
 #define MAX_LEN_BUFFER 1024
 #define QUIT_QUERY "QUIT"
 
-bool FLAG_CONTINUE = true;
+bool is_from_file = false;
 char BUFFER[MAX_LEN_BUFFER];
 char CORRECT_BUFFER[MAX_LEN_BUFFER];
 char QUERY[MAX_LEN_BUFFER];
 
-//strtok for correct string
+
+bool is_end_session() {
+    if (strncasecmp(QUERY, QUIT_QUERY, strlen(QUIT_QUERY)) == 0) {
+        return true;
+    }
+    return false;
+}
+
+void proccesing_query(int __fd_client, bool* flag_end) {
+    write(__fd_client, QUERY, MAX_LEN_BUFFER);
+    *flag_end = is_end_session();
+    ssize_t bytes_read = recv(__fd_client, QUERY, MAX_LEN_BUFFER, 0);
+    QUERY[bytes_read] = '\0';
+    if (bytes_read > 0) {
+        fputs(QUERY, stdout);
+    } else {
+        fprintf(stderr, "The server dropped the connection");
+        close(__fd_client);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void processing_query_from_file(int __fd_client, bool* flag_end) {
+    FILE* file = fopen(QUERY + 1, "r");
+    if (file == NULL) {
+        printf("File is not found.\n");
+        return;
+    }
+    while(!feof(file)) {
+      //  memset(QUERY, '\0', strlen(QUERY));
+        fgets(QUERY, MAX_LEN_BUFFER, file);
+        printf("> %s", QUERY);
+        proccesing_query(__fd_client, flag_end);
+        if (*flag_end == true) {
+            return;
+        }
+    }
+}
+
 
 int main(int argc, char* argv[])
 {
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     struct addrinfo hints = {0};
     struct addrinfo* result = NULL;
 
@@ -29,7 +70,7 @@ int main(int argc, char* argv[])
     hints.ai_protocol = 0;
     hints.ai_flags = 0;
 
-    int s = getaddrinfo(NULL, "8080", &hints, &result);
+    int s = getaddrinfo(NULL, argv[1], &hints, &result);
 
     if (s != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
@@ -47,21 +88,15 @@ int main(int argc, char* argv[])
     read(fd_client, BUFFER, MAX_LEN_BUFFER);
     printf("%s", BUFFER);
 
-
-    while(FLAG_CONTINUE) {
+    bool flag_end = false;
+    while(flag_end == false) {
         printf("> ");
         fgets(QUERY,MAX_LEN_BUFFER, stdin);
-        write(fd_client, QUERY, MAX_LEN_BUFFER);
-        if (strncasecmp(QUERY, QUIT_QUERY, strlen(QUIT_QUERY)) == 0) {
-            FLAG_CONTINUE = false;
-        }
-        ssize_t bytes_read = recv(fd_client, QUERY, MAX_LEN_BUFFER, 0);
-        QUERY[bytes_read] = '\0';
-        if (bytes_read > 0) {
-            fputs(QUERY, stdout);
+        if (QUERY[0] == '@') {
+            QUERY[strlen(QUERY) - 1] = '\0';
+            processing_query_from_file(fd_client, &flag_end);
         } else {
-            fprintf(stderr, "The server dropped the connection");
-            exit(EXIT_FAILURE);
+            proccesing_query(fd_client, &flag_end);
         }
     }
 
